66f492eaf29e00b2a1e77ea309316ea66746a830
sangsangsang369 <parkse0324@gmail.com>
2022-06-30 17:20:02 +0900

    Snap Scroll UI & Volume Slider

---
+++ Assets/Scripts/MainScene/ScrollMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollMenu : MonoBehaviour
+{
+    public GameObject scrollbar;
+    float scroll_pos = 0;
+    float[] pos;
+    float distance;
+
+    private void Start() 
+    {
+        pos = new float[transform.childCount];
+        distance = 1f / (pos.Length - 1f);
+        for (int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = distance * i;
+        }
+    }
+
+    private void Update() 
+    {
+        if (Input.GetMouseButton(0))
+        {
+            scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+        }
+        else
+        {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+                {
+                    scrollbar.GetComponent<Scrollbar>().value 
+                    = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
+                }
+            }
+        }
+
+        for (int i = 0; i < pos.Length; i++)
+        {
+            if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+            {
+                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+                for (int a = 0; a < pos.Length; a++)
+                {
+                    if (a != i)
+                    {
+                        transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.9f, 0.9f), 0.1f);
+                    }
+                }
+            }
+        }
+    }
+}
